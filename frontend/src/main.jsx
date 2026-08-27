838a8be250d3c97523395a991ae9cabe96f39700
sagar <sagarpanwer11@gmail.com>
2026-08-27 18:15:37 +0530

    Created VAriables file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1>PickWise - AI</h1>;
-}
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
