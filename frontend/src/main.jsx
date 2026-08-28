180dca8ace6dc7b2031490579f155ef4bc481bf3
sagar <sagarpanwer11@gmail.com>
2026-08-28 13:33:00 +0530

    Created Home file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "./styles/variables.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
