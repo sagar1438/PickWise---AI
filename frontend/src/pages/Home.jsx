232348a04a51225edc62425811265c66c618baff
sagar <sagarpanwer11@gmail.com>
2026-08-31 13:05:34 +0530

    Modified home file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
+import Navbar from "../components/Navbar";
+
 function Home() {
   return (
-    <main>
-      <h1>PickWise - AI</h1>
-      <p>Find the right AI model for your project.</p>
-    </main>
+    <>
+      <Navbar />
+
+      <main>
+        <section>
+          <p>MODEL MATCH</p>
+          <h1>Find the right AI model for your project.</h1>
+          <p>
+            Describe what you're building and get personalized AI model
+            recommendations.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
 
