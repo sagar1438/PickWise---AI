ad41a9ec4dfac235e8f13bed8d9d0ff84de709a5
sagar <sagarpanwer11@gmail.com>
2026-08-31 14:32:42 +0530

    Load shared state styles in app entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./styles/variables.css";
 import "./styles/global.css";
+import "./styles/states.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
