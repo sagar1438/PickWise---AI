a5badfa5729099e82c21078d81b83954bf55cb68
sagar <sagarpanwer11@gmail.com>
2026-09-01 23:56:51 +0530

    Added initial model discovery page and filters

---
+++ frontend/src/pages/Discover.jsx
@@ -0,0 +1,8 @@
+import { useMemo, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import SearchBar from "../components/SearchBar";
+import FilterBar from "../components/FilterBar";
+import ModelGrid from "../components/ModelGrid";
+import EmptyState from "../components/EmptyState";
+
