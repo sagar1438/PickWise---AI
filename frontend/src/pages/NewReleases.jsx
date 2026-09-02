49fdd3cddcb23fd99b69ba43d2be5d2952077f24
sagar <sagarpanwer11@gmail.com>
2026-09-02 13:14:28 +0530

    Added new release page with model demo data

---
+++ frontend/src/pages/NewReleases.jsx
@@ -0,0 +1,6 @@
+import { CalendarDays, ArrowUpRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ModelBadge from "../components/ModelBadge";
+
