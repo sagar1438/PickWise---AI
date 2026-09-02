fd634577329a630c72f588e0bf2baae3d6bb71da
sagar <sagarpanwer11@gmail.com>
2026-09-02 13:44:20 +0530

    Added initial model comparison page

---
+++ frontend/src/pages/Compare.jsx
@@ -0,0 +1,6 @@
+import { Plus, X } from "lucide-react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ModelBadge from "../components/ModelBadge";
+import Button from "../components/Button";
+
