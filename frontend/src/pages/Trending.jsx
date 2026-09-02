3695fb4393c723ee2e2f1e2a3648b112495f9260
sagar <sagarpanwer11@gmail.com>
2026-09-02 13:03:23 +0530

    Added initial trending model

---
+++ frontend/src/pages/Trending.jsx
@@ -0,0 +1,7 @@
+import { TrendingUp } from "lucide-react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ModelBadge from "../components/ModelBadge";
+
+
+
